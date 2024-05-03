32a592c88fa704a49fbec5d6c8d8635bbbc6ad9b
Waseem Ahmad <72740911+WaseemAhmad9977@users.noreply.github.com>
2024-05-03 17:34:43 +0530

    random color project

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
-import Accordian from './components';
-import Index1 from './components/Index1';
+import Index1 from './components/accordian/Index1';
+import RandomColor from './components/random-color';
+
 
 function App() {
   return (
     <div className="App">
       {/* accordian components */}
       {/* <Accordian></Accordian> */}
-      <Index1></Index1>
+      {/* <Index1></Index1> */}
+      <RandomColor></RandomColor>
+    
     </div>
   );
 }
